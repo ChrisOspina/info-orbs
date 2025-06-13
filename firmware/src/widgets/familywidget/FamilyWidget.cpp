3b6299b3bb7f628d0deb0ce378432562cf4c4714
ChrisOspina <ChrisOspina99@gmail.com>
2025-06-13 16:27:23 -0400

    setup methods implemented

--- firmware/src/widgets/familywidget/FamilyWidget.cpp
+++ firmware/src/widgets/familywidget/FamilyWidget.cpp
@@ -9,7 +9,36 @@ FamilyWidget::FamilyWidget(ScreenManager &manager) : Widget(manager) {
 FamilyWidget::~FamilyWidget(){
 }
 
-void FamilyWidget::draw(bool force){
-  
+void FamilyWidget::setup() {
+  m_time = GlobalTime::getInstance();
+  #ifdef FAMILY_SCREEN_MODE
+      m_screenMode = FAMILY_SCREEN_MODE;
+  #endif
+  configureColors();
 }
+
+
+
+void FamilyWidget::draw(bool force){
+      //TODO figure out how to implement method
+}
+
+void FamilyWidget::configureColors() {
+    m_foregroundColor = m_screenMode == Light ? TFT_BLACK : TFT_WHITE;
+    m_backgroundColor = m_screenMode == Light ? TFT_WHITE : TFT_BLACK;
+
+    // NOTE: In Light mode, we draw decorative black chunks and display the high and low on them in white.
+    //       It does not make sense to have glaring white chunks in dark mode, so we don't draw them at all,
+    //       and display the high and low in white too.
+    m_invertedForegroundColor = m_screenMode == Light ? m_backgroundColor : m_foregroundColor;
+    m_invertedBackgroundColor = m_screenMode == Light ? m_foregroundColor : m_backgroundColor;
+
+    m_manager.setBackgroundColor(m_backgroundColor);
+}
+
+String FamilyWidget::getName(){
+  return "family";
+}
+
+
 
--- firmware/src/widgets/familywidget/FamilyWidget.h
+++ firmware/src/widgets/familywidget/FamilyWidget.h
@@ -2,7 +2,10 @@
 #define FAMILY_WIDGET_H
 
 #include "Widget.h"
+#include "Utils.h"
 #include "config_helper.h"
+#include "GlobalTime.h"
+
 
 class FamilyWidget: public Widget{
     public:
@@ -12,12 +15,24 @@ class FamilyWidget: public Widget{
         void setup() override;
         void draw(bool force = false) override;
         String getName() override;
-        bool isVisible() const;
-        void setName(const std::string& name);
-        void setVisibility(bool visible);
-        
+
     private:
         void displayImage(int displayIndex, const String &digit);
+        void configureColors();
+
+        GlobalTime *m_time;
+        int8_t m_mode;
+        String name;
+        bool visible;
+
+        ScreenMode m_screenMode = Dark;
+        uint16_t m_foregroundColor;
+        uint16_t m_backgroundColor;
+        uint16_t m_invertedForegroundColor;
+        uint16_t m_invertedBackgroundColor;
+
+        ScreenMode m_screenMode = Dark;
+        const int centre = 120; // Centre location of the screen(240x240)
 };
 
 
